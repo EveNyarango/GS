3681f26696f3e634078e9ffd9361d473ef1a76f2
EveNyarango <evenyarango@gmail.com>
2021-04-14 02:47:12 +0300

    add bottom navigation view

--- app/src/main/java/com/example/green/MainActivity.java
+++ app/src/main/java/com/example/green/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.green;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,16 +11,55 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.google.android.material.bottomnavigation.BottomNavigationMenu;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        auth = FirebaseAuth.getInstance();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(onNav);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.frameMain,
+                new Fragment1()).commit();
 
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener onNav = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selected = null;
+
+            switch (item.getItemId()){
+                case R.id.btmProfile:
+                    selected = new Fragment1();
+                    break;
+
+                case R.id.btmAsk:
+                    selected = new Fragment2();
+                    break;
+
+                case R.id.btmRequest:
+                    selected = new Fragment3();
+                    break;
+
+                case R.id.btmHome:
+                    selected = new Fragment4();
+                    break;
+
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.frameMain,
+                    selected).commit();
+
+            return true;
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
