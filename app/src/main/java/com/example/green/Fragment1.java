1fd91e60a48ae2f6fb9e2623e5c87da759c11e81
EveNyarango <evenyarango@gmail.com>
2021-05-09 17:09:29 +0300

    fuctionality for retrieving user profile data from firestone

--- app/src/main/java/com/example/green/Fragment1.java
+++ app/src/main/java/com/example/green/Fragment1.java
@@ -11,8 +11,21 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class Fragment1 extends Fragment implements View.OnClickListener{
     @Nullable
+    CircleImageView IvImage;
+    TextView nameTv, profTv, bioTv, emailTv, webTv;
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment1, container, false);
@@ -22,19 +35,66 @@ public class Fragment1 extends Fragment implements View.OnClickListener{
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        TextView textView = getActivity().findViewById(R.id.tvProf1);
 
-        textView.setOnClickListener(this);
+        IvImage = getActivity().findViewById(R.id.ivProfile_f1);
+        nameTv = getActivity().findViewById(R.id.tvName_f1);
+        profTv = getActivity().findViewById(R.id.tvProf_f1);
+        bioTv = getActivity().findViewById(R.id.tvBio_f1);
+        emailTv = getActivity().findViewById(R.id.tvEmail_f1);
+        webTv = getActivity().findViewById(R.id.tvWebsite_f1);
+
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()){
-            case R.id.tvProf1:
-            Intent intent = new Intent(getActivity(), ProfileActivity.class);
-            startActivity(intent);
+
 
         }
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String currentid = user.getUid();
+        DocumentReference reference;
+        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+
+        reference = firestore.collection("user").document(currentid);
+
+        reference.get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+
+                        if (task.getResult().exists()){
+                            String nameResult = task.getResult().getString("name");
+                            String bioResult = task.getResult().getString("bio");
+                            String emailResult = task.getResult().getString("email");
+                            String webResult = task.getResult().getString("web");
+                            String profResult = task.getResult().getString("prof");
+                            String url = task.getResult().getString("url");
+
+                            Picasso.get().load(url).into(IvImage);
+                            nameTv.setText(nameResult);
+                            bioTv.setText(bioResult);
+                            emailTv.setText(emailResult);
+                            webTv.setText(webResult);
+                            profTv.setText(profResult);
+
+
+
+
+                        }else{
+                            Intent intent = new Intent(getActivity(),ProfileActivity.class);
+                            startActivity(intent);
+                        }
+
+                    }
+                });
 
     }
 }
