062a44a137daf0d3beadda50f9d5144408c850ed
Annewangari <annekare3@gmail.com>
2021-05-16 15:29:15 +0300

    delete picture fuctionality

--- app/src/main/java/com/example/green/BottomNav.java
+++ app/src/main/java/com/example/green/BottomNav.java
@@ -13,12 +13,25 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.cardview.widget.CardView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import static android.os.Build.VERSION_CODES.O;
 
 public class BottomNav extends BottomSheetDialogFragment {
 
@@ -27,10 +40,13 @@ public class BottomNav extends BottomSheetDialogFragment {
     DocumentReference reference;
     CardView cvLogout, cvPrivacy, cvDelete;
     FirebaseAuth mAuth;
+    DatabaseReference df;
+    String url;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = getLayoutInflater().inflate(R.layout.bottom_nav, null);
+        df= FirebaseDatabase.getInstance().getReference("all users");
 
         cvLogout = view.findViewById(R.id.cvLogout);
         cvPrivacy = view.findViewById(R.id.cvPrivacy);
@@ -41,6 +57,19 @@ public class BottomNav extends BottomSheetDialogFragment {
         String currentid = user.getUid();
 
         reference = db.collection("user").document(currentid);
+        reference.get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if(task.getResult().exists()){
+                            url = task.getResult().getString("url");
+                        }else{
+//                            empty
+
+                        }
+
+                    }
+                });
         FirebaseUser mCurrentUser = mAuth.getCurrentUser();
 
         cvLogout.setOnClickListener(new View.OnClickListener() {
@@ -73,7 +102,32 @@ public class BottomNav extends BottomSheetDialogFragment {
                                         .addOnSuccessListener(new OnSuccessListener<Void>() {
                                             @Override
                                             public void onSuccess(Void aVoid) {
-                                                Toast.makeText(getActivity(), "Deleted", Toast.LENGTH_SHORT).show();
+
+                                                Query query = df.orderByChild("uid").equalTo(currentid);
+                                                query.addListenerForSingleValueEvent(new ValueEventListener() {
+
+                                                    @Override
+                                                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                                        for(DataSnapshot dataSnapshot : snapshot.getChildren()){
+                                                            dataSnapshot.getRef().removeValue();
+                                                        }
+
+                                                    }
+
+                                                    @Override
+                                                    public void onCancelled(@NonNull DatabaseError error) {
+
+                                                    }
+                                                });
+                                                StorageReference ref = FirebaseStorage.getInstance().getReferenceFromUrl(url);
+                                                ref.delete()
+                                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                            @Override
+                                                            public void onComplete(@NonNull Task<Void> task) {
+                                                                Toast.makeText(getActivity(), "Deleted", Toast.LENGTH_SHORT).show();
+                                                            }
+                                                        });
+
                                             }
                                         });
 
